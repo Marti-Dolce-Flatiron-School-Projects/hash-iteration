cd2da0f843086623481ae1d21bcdd11bee8a74f8
Martinique <me@martidolce.com>
2020-11-14 16:33:00 -0500

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,22 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ''
+  passengers.each do |suite, name|
+    winner = name if suite == :suite_a && name.start_with?('A')
+  end
+  winner
 end
